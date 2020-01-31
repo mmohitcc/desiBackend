570f3680ef69c1168d0dec68c59ce8e3b7c6345d
Mohit <thewush@hotmail.com>
2020-01-30 23:01:58 -0700

    Hopefully fix memory leak
    opening and closing chrome browser to help with memory leaks

--- scrape.py
+++ scrape.py
@@ -8,12 +8,14 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 #driver = webdriver.Chrome()
 
 
 
 def scrape(link):
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+	# driver.execute_script("window.open('"+link+"', '_blank')")
 	driver.get(link)
 	# time.sleep(5)
 	headlines = driver.find_elements_by_xpath("//*[contains(text(), 'VKprime')]")[0]
@@ -28,28 +30,29 @@ def scrape(link):
 		allLinks.append(l.get_attribute("href"))
 	sources = []
 	for l in allLinks:
-		print (l)
+		# print (l)
 		driver.get(l)
 		# frame = driver.find_element_by_xpath("//iframe[contains(@src,'vkprime'])]")
 		ifras = driver.find_elements_by_tag_name("iframe")
-		print("found frames \n")
+		# print("found frames \n")
 		for ifra in ifras:
-			print("checking frames")
+			# print("checking frames")
 			if(ifra.get_attribute("src").find("vk") > 0):
 				sources.append(ifra.get_attribute("src"))
 				
 
 
-	for source in sources:
-		print(source)
+	# for source in sources:
+	# 	# print(source)
 	
 	frames = []
 	for source in sources:
 		frames.append('<iframe src="%s" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(source))
-
+	driver.quit()
 	return (frames)
 
-def scrapeSecond(): 
+def scrapeSecond():
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	driver.get("https://www.desitellybox.me/category/and-tv/bhabhiji-ghar-par-hain/")
 	headlines = driver.find_elements_by_xpath("//*[contains(text(), 'Episode Watch Online')]")[0].click()
 	time.sleep(5)
@@ -59,7 +62,7 @@ def scrapeSecond():
 		link = (headline.get_attribute("href"))
 		if (link.lower().find("vk") > 0 ):
 			links.append(link)
-			print (link)
+			# print (link)
 
 	if(len(links) == 0):
 		return 'Latest Episode not available yet'
@@ -67,11 +70,13 @@ def scrapeSecond():
 	eyeD = link[link.find("=")+1: len(link)]
 	linkTwo = links[1]
 	eyeDTwo = linkTwo[linkTwo.find("=")+1: len(linkTwo)]
-	print (eyeD)
-	print (eyeDTwo)
+	# print (eyeD)
+	# print (eyeDTwo)
+	driver.quit()
 	return (['<iframe src="http://vkprime.com/embed-%s.html" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(eyeD), '<iframe src="http://vkprime.com/embed-%s.html" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(eyeDTwo)])
 
-def scrapeThird(): 
+def scrapeThird():
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	driver.get("https://www.desitellybox.me/category/star-plus/kullfi-kumarr-bajewala/")
 	headlines = driver.find_elements_by_xpath("//*[contains(text(), '2020 Watch Online')]")[0].click()
 	time.sleep(5)
@@ -81,7 +86,7 @@ def scrapeThird():
 		link = (headline.get_attribute("href"))
 		if (link.lower().find("vk") > 0 ):
 			links.append(link)
-			print (link)
+			# print (link)
 
 	if(len(links) == 0):
 		return 'Latest Episode not available yet'
@@ -89,11 +94,13 @@ def scrapeThird():
 	eyeD = link[link.find("=")+1: len(link)]
 	linkTwo = links[1]
 	eyeDTwo = linkTwo[linkTwo.find("=")+1: len(linkTwo)]
-	print (eyeD)
-	print (eyeDTwo)
+	# print (eyeD)
+	# print (eyeDTwo)
+	driver.quit()
 	return (['<iframe src="http://vkprime.com/embed-%s.html" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(eyeD), '<iframe src="http://vkprime.com/embed-%s.html" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(eyeDTwo)])
 
-def scrapeFourth(): 
+def scrapeFourth():
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	driver.get("https://www.desitellybox.me/category/sony-tv/kapil-sharma-show/")
 	headlines = driver.find_elements_by_xpath("//*[contains(text(), 'Episode Watch Online')]")[0].click()
 	time.sleep(5)
@@ -103,15 +110,17 @@ def scrapeFourth():
 		link = (headline.get_attribute("href"))
 		if (link.lower().find("vk") > 0 ):
 			links.append(link)
-			print (link)
+			# print (link)
 
 	if(len(links) == 0):
 		return 'Latest Episode not available yet'
 	link = links[-1]
 	eyeD = link[link.find("=")+1: len(link)]
+	driver.quit()
 	return (['<iframe src="http://vkprime.com/embed-%s.html" frameborder="0" allowfullscreen="" marginwidth="0" marginheight="0" scrolling="NO" width="520" height="400"></iframe>'%(eyeD)])
 
 def getListOfShows():
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	showLinks = [
 	#"http://www.yodesitv.info/star-plus/",
 	#"http://www.yodesitv.info/colors/",
@@ -146,11 +155,12 @@ def getListOfShows():
 			show["imageLink"] = (card.find_element_by_tag_name("a").find_element_by_tag_name("img").get_attribute("src"))
 			if((len(name) != 0)):
 				allShows["allShows"].append(show)
-		print(len(showCards))
-
+		# print(len(showCards))
+	driver.quit()
 	return allShows
 
 def getDates(link):
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	driver.get(link)
 	headlines = driver.find_elements_by_class_name("latestPost-content")
 	eppisodes = {"eppisodes": []}
@@ -159,8 +169,8 @@ def getDates(link):
 		link = headline.find_element_by_tag_name("a").get_attribute("href")
 		eppisode = {"title": title, "link": link}
 		eppisodes["eppisodes"].append(eppisode)
-		print(title)
-
+		# print(title)
+	driver.quit()
 	return eppisodes
 
 
